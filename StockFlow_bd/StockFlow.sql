87568ba20373b4445b72e315c819e156e60f73e4
Alejandro Ribera <132957242+AlejandroRibera1987@users.noreply.github.com>
2024-05-19 19:13:25 -0300

    Actualizacion de BD

--- StockFlow_bd/StockFlow.sql
+++ StockFlow_bd/StockFlow.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 11-03-2024 a las 03:49:39
+-- Tiempo de generación: 19-05-2024 a las 23:56:01
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -44,7 +44,10 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`idcliente`, `dni`, `nombre`, `telefono`, `correo`, `direccion`, `dateadd`, `usuario_id`, `estatus`) VALUES
-(1, 32890754, 'Alejandro RIbera', 1155259236, 'alejandroribera1987@gmail.com', 'Deheza 3145', '2024-03-10 17:30:37', 1, 1);
+(1, 32890754, 'Alejandro RIbera', 1155259236, 'alejandroribera1987@gmail.com', 'Deheza 3145', '2024-03-10 17:30:37', 1, 1),
+(2, 3289754, 'Danko', 12345, 'Danko@gmail.com', 'Mariano Acha 1234', '2024-03-15 18:46:34', 1, 1),
+(3, 1, 'Cliente Prueba', 123456, 'prueba@prueba.com', '', '2024-03-15 20:43:48', 1, 0),
+(4, 0, 'Jorge', 1872636, 'casa|@sks.com', '', '2024-03-15 20:48:36', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -85,8 +88,22 @@ CREATE TABLE `entradas` (
   `codproducto` int(11) NOT NULL,
   `fecha` datetime NOT NULL DEFAULT current_timestamp(),
   `cantidad` int(11) NOT NULL,
-  `precio` decimal(10,2) NOT NULL
+  `precio` decimal(10,2) NOT NULL,
+  `usuario_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `entradas`
+--
+
+INSERT INTO `entradas` (`correlativo`, `codproducto`, `fecha`, `cantidad`, `precio`, `usuario_id`) VALUES
+(5, 11, '2024-03-16 18:03:49', 123, 1231.00, 1),
+(6, 12, '2024-03-16 18:09:14', 2, 1500002.00, 1),
+(7, 15, '2024-03-16 19:55:40', 2, 150.00, 1),
+(8, 22, '2024-03-17 12:17:21', 12, 123.00, 1),
+(9, 23, '2024-03-17 12:17:54', 1, 1231.00, 1),
+(10, 24, '2024-03-17 12:24:02', 1, 1746.00, 1),
+(11, 25, '2024-03-17 12:25:01', 19, 18376.00, 1);
 
 -- --------------------------------------------------------
 
@@ -114,8 +131,36 @@ CREATE TABLE `producto` (
   `proveedor` int(11) DEFAULT NULL,
   `precio` decimal(10,2) DEFAULT NULL,
   `existencia` int(11) DEFAULT NULL,
+  `date_add` datetime NOT NULL DEFAULT current_timestamp(),
+  `usuario_id` int(11) NOT NULL,
+  `estatus` int(11) NOT NULL DEFAULT 1,
+  `codbarra` text DEFAULT NULL,
   `foto` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`codproducto`, `descripcion`, `proveedor`, `precio`, `existencia`, `date_add`, `usuario_id`, `estatus`, `codbarra`, `foto`) VALUES
+(11, 'juntoa', 6, 1231.00, 123, '2024-03-16 18:03:49', 1, 1, 'junta.jpg', NULL),
+(12, 'Moto zanella', 3, 1500002.00, 2, '2024-03-16 18:09:14', 1, 1, '12632-255', NULL),
+(15, 'ruedas', 11, 150.00, 2, '2024-03-16 19:55:40', 1, 1, '15-526', NULL),
+(22, 'asf', 7, 123.00, 12, '2024-03-17 12:17:21', 1, 1, '213123', NULL),
+(23, 'mundo', 4, 1231.00, 1, '2024-03-17 12:17:54', 1, 1, '125267', NULL),
+(24, 'cubierta', 7, 1746.00, 1, '2024-03-17 12:24:02', 1, 1, '29869-2516', NULL),
+(25, 'juntas', 11, 18376.00, 19, '2024-03-17 12:25:01', 1, 1, '8755-55', NULL);
+
+--
+-- Disparadores `producto`
+--
+DELIMITER $$
+CREATE TRIGGER `entradas_A_I` AFTER INSERT ON `producto` FOR EACH ROW BEGIN	
+        	INSERT INTO entradas(codproducto,cantidad,precio,usuario_id)
+            VALUES (new.codproducto,new.existencia,new.precio,new.usuario_id);
+        END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -128,25 +173,33 @@ CREATE TABLE `proveedor` (
   `proveedor` varchar(100) DEFAULT NULL,
   `contacto` varchar(100) DEFAULT NULL,
   `telefono` bigint(11) DEFAULT NULL,
-  `direccion` text DEFAULT NULL
+  `correo` varchar(200) DEFAULT NULL,
+  `direccion` text DEFAULT NULL,
+  `date_add` datetime NOT NULL DEFAULT current_timestamp(),
+  `usuario_id` int(11) NOT NULL,
+  `estatus` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Volcado de datos para la tabla `proveedor`
 --
 
-INSERT INTO `proveedor` (`codproveedor`, `proveedor`, `contacto`, `telefono`, `direccion`) VALUES
-(1, 'BIC', 'Claudia Rosales', 789877889, 'Avenida las Americas'),
-(2, 'CASIO', 'Jorge Herrera', 565656565656, 'Calzada Las Flores'),
-(3, 'Omega', 'Julio Estrada', 982877489, 'Avenida Elena Zona 4, Guatemala'),
-(4, 'Dell Compani', 'Roberto Estrada', 2147483647, 'Guatemala, Guatemala'),
-(5, 'Olimpia S.A', 'Elena Franco Morales', 564535676, '5ta. Avenida Zona 4 Ciudad'),
-(6, 'Oster', 'Fernando Guerra', 78987678, 'Calzada La Paz, Guatemala'),
-(7, 'ACELTECSA S.A', 'Ruben PÃ©rez', 789879889, 'Colonia las Victorias'),
-(8, 'Sony', 'Julieta Contreras', 89476787, 'Antigua Guatemala'),
-(9, 'VAIO', 'Felix Arnoldo Rojas', 476378276, 'Avenida las Americas Zona 13'),
-(10, 'SUMAR', 'Oscar Maldonado', 788376787, 'Colonia San Jose, Zona 5 Guatemala'),
-(11, 'HP', 'Angel Cardona', 2147483647, '5ta. calle zona 4 Guatemala');
+INSERT INTO `proveedor` (`codproveedor`, `proveedor`, `contacto`, `telefono`, `correo`, `direccion`, `date_add`, `usuario_id`, `estatus`) VALUES
+(1, 'BIC', 'Claudia Rosales', 789877889, '', 'Avenida las Americas', '2024-03-16 14:01:58', 1, 0),
+(2, 'CASIO', 'Jorge Herrera', 565656565656, '', 'Calzada Las Flores', '2024-03-16 14:01:58', 1, 0),
+(3, 'Omega', 'Julio Estrada', 123456789202563, 'mail@gmail.com', 'Avenida Elena Zona 4, Guatemala', '2024-03-16 14:01:58', 1, 1),
+(4, 'Dell Compani', 'Roberto Estrada', 2147483647, '', 'Guatemala, Guatemala', '2024-03-16 14:01:58', 1, 1),
+(5, 'Olimpia S.A', 'Elena Franco Morales', 564535676, '', '5ta. Avenida Zona 4 Ciudad', '2024-03-16 14:01:58', 1, 1),
+(6, 'Oster', 'Fernando Guerra', 78987678, '', 'Calzada La Paz, Guatemala', '2024-03-16 14:01:58', 1, 1),
+(7, 'ACELTECSA S.A', 'Ruben PÃ©rez', 789879889, '', 'Colonia las Victorias', '2024-03-16 14:01:58', 1, 1),
+(8, 'Sony', 'Julieta Contreras', 89476787, '', 'Antigua Guatemala', '2024-03-16 14:01:58', 1, 1),
+(9, 'VAIO', 'Felix Arnoldo Rojas', 476378276, '', 'Avenida las Americas Zona 13', '2024-03-16 14:01:58', 1, 1),
+(10, 'SUMAR', 'Oscar Maldonado', 788376787, '', 'Colonia San Jose, Zona 5 Guatemala', '2024-03-16 14:01:58', 1, 1),
+(11, 'HP', 'Angel Cardona', 2147483647, '', '5ta. calle zona 4 Guatemala', '2024-03-16 14:01:58', 1, 1),
+(12, 'Soluciones IT', 'Alejandro RIbera', 1155259236, 'alejandroribera1987@gmail.com', '', '2024-03-16 15:38:27', 1, 1),
+(13, 'Aysa SA', 'Javier', 5165198154, 'Aysa@gmail.com', '', '2024-03-16 19:16:44', 1, 1),
+(14, 'edenor', 'juan', 1847420, 'juan@gmail.com', '', '2024-03-16 20:02:02', 1, 1),
+(15, 'prueba nuevo', 'julian', 1566463, 'prueba1@gmail.com', '', '2024-03-17 12:28:53', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -189,10 +242,13 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`idusuario`, `nombre`, `correo`, `usuario`, `clave`, `rol`, `estatus`) VALUES
-(1, 'Alejandro', 'alejandroribera1987@gmail.com', 'admin', '29ee44212aac91ae87f7f43a8b75c6ae', 1, 1),
-(2, 'Prueba', 'prueba1@gmail.com.ar', 'Cliente_Prueba', '202cb962ac59075b964b07152d234b70', 3, 1),
-(3, 'Prueba_2', 'prueba2@gmail.com', 'Prueba2asda', 'adc69151a8fa1cfc21fbeda84b2a9cd8', 3, 1),
-(5, 'Lucrecia ', 'sg.lucracia@gmail.com', 'Lukre', 'eb8ea2656f3b2c6963fff0b5b9b136af', 2, 1);
+(1, 'Alejandro Ribera', 'alejandroribera1987@gmail.com', 'admin', '29ee44212aac91ae87f7f43a8b75c6ae', 1, 1),
+(2, 'Mario Danko', 'prueba1@gmail.com.ar', 'Danko', 'e10adc3949ba59abbe56e057f20f883e', 2, 1),
+(3, 'Prueba_2', 'prueba2@gmail.com', 'Pat', '81dc9bdb52d04dc20036dbd8313ed055', 3, 1),
+(5, 'Lucrecia ', 'sg.lucracia@gmail.com', 'Lucrecia', '5afb8cb79ee250b5409df42f4a5213b6', 2, 1),
+(6, 'Fernando', 'casa|@sks.com', 'Fernando', 'e10adc3949ba59abbe56e057f20f883e', 3, 0),
+(7, 'Julieta', 'adkno@gmail.com', 'Julieta', 'e10adc3949ba59abbe56e057f20f883e', 3, 1),
+(8, 'Claudio', 'claudio@gmail.com', 'Claudio', 'e10adc3949ba59abbe56e057f20f883e', 3, 1);
 
 --
 -- Índices para tablas volcadas
@@ -241,13 +297,15 @@ ALTER TABLE `factura`
 --
 ALTER TABLE `producto`
   ADD PRIMARY KEY (`codproducto`),
-  ADD KEY `proveedor` (`proveedor`);
+  ADD KEY `proveedor` (`proveedor`),
+  ADD KEY `usuario_id` (`usuario_id`);
 
 --
 -- Indices de la tabla `proveedor`
 --
 ALTER TABLE `proveedor`
-  ADD PRIMARY KEY (`codproveedor`);
+  ADD PRIMARY KEY (`codproveedor`),
+  ADD KEY `usuario_id` (`usuario_id`);
 
 --
 -- Indices de la tabla `rol`
@@ -270,7 +328,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `detallefactura`
@@ -288,7 +346,7 @@ ALTER TABLE `detalle_temp`
 -- AUTO_INCREMENT de la tabla `entradas`
 --
 ALTER TABLE `entradas`
-  MODIFY `correlativo` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `correlativo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `factura`
@@ -300,13 +358,13 @@ ALTER TABLE `factura`
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `codproducto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `codproducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `proveedor`
 --
 ALTER TABLE `proveedor`
-  MODIFY `codproveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `codproveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `rol`
@@ -318,7 +376,7 @@ ALTER TABLE `rol`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restricciones para tablas volcadas
@@ -361,7 +419,14 @@ ALTER TABLE `factura`
 -- Filtros para la tabla `producto`
 --
 ALTER TABLE `producto`
-  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`proveedor`) REFERENCES `proveedor` (`codproveedor`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`proveedor`) REFERENCES `proveedor` (`codproveedor`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `producto_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`idusuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD CONSTRAINT `proveedor_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`idusuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `usuario`
