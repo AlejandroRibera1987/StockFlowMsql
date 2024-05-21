1582ecfbdd52434ed44e4ac2d84c01bfb65cb692
AlejandroRibera1987 <alejandroribera1987@gmail.com>
2024-05-21 12:14:04 -0300

    Actualizado Index.html

--- sistema/index.php
+++ sistema/index.php
@@ -2,6 +2,9 @@
  
  session_start();
 
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,35 +16,42 @@
 <body>
 	<?php include "include/header.php" ?>
 	<section id="container">
-		<h1>Bienvenido al sistema</h1>
-		<h2>Dashboard</h2>
+		<h1 class="dashboard_titulo">Bienvenide <?php echo $_SESSION['user']?></h1>
+		<h2 class="dashboard_subtitulo">Dashboard</h2>
 		<div class="dashboard">
 			<div class="dashboard_1">
-				<div class="usuarios">
+				<div class="usuarios items">
 					<h2><a href="lista_usuarios.php">Usuarios</a></h2>
-					<p>activos: <span>4</span></p>
-					<p>Inactivos: <span>0</span></p>
+					<p class="items_text">activos: <span>4</span></p>
+					<p class="items_text">Inactivos: <span>0</span></p>
 				</div>
 
-				<div class="clientes">
+				<div class="clientes items ">
 					<h2><a href="#">Clientes</a></h2>
-					<p>Cantidad de Clientes: <span>1</span></p>
+					<p class="items_text">Activos: <span>1</span></p>
+					<p class="items_text">Inactivos: <span>1</span></p>
+				</div>
+				<div class="proveedores items">
+					<h2><a href="#">proveedores</a></h2>
+					<p class="items_text">Activos: <span>12</span></p>
+					<p class="items_text">Inactivos: <span>1</span></p>
 				</div>
 			</div>
 			<div class="dashboard_2">
-				<div class="productos">
+				<div class="productos items">
 					<h2><a href="#">Productos</a></h2>
-					<p>Productos Activos: <span>100</span></p>
-					<p>Productos Inactivos: <span>0</span></p>
+					<p class="items_text">Productos Activos: <span>100</span></p>
+					<p class="items_text">Productos con bajo Stock: <span>2</span></p>
+					<p class="items_text">Productos Inactivos: <span>0</span></p>
 				</div>
 
-				<div class="facturacion">
+				<div class="facturacion items">
 					<h2><a href="#">Balance</a></h2>
-					<p>Facturacion del dia: <span>$29390</span></p>
-					<p>Gastos del dia: <span>$29390</span></p>
-					<p>Pagos en Efectivos: <span>$32029</span></p>
-					<p>Pagos con debito: <span>$32029</span></p>
-					<p>Pagos con Credito: <span>$32029</span></p>
+					<p class="items_text">Facturacion del dia: <span>$29390</span></p>
+					<p class="items_text">Gastos del dia: <span>$29390</span></p>
+					<p class="items_text">Pagos en Efectivos: <span>$32029</span></p>
+					<p class="items_text">Pagos con debito: <span>$32029</span></p>
+					<p class="items_text">Pagos con Credito: <span>$32029</span></p>
 				</div>
 			</div>
 		</div>
